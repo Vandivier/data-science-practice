2034f1f4c5a4a1ea7ee6ea6dd83f811978336e92
Vandivier <vandivier_john@yahoo.com>
2016-11-08 18:37:44 -0500

    migrant remittance mapped to is_oecd dummy;

---
+++ sql/migrant-remittances/migrant-remittances.sql
@@ -0,0 +1,26 @@
+CREATE SCHEMA `migrant_remittances`;
+
+/*
+Import oecd.csv and outflows.csv
+transform outflows.csv to migrant_remittances_in.csv
+*/
+
+ALTER TABLE `migrant_remittances`.`migrant_remittances_in` 
+ADD COLUMN `is_oecd` INT(1) NULL AFTER `after`;
+
+UPDATE migrant_remittances.migrant_remittances_in AS indata
+SET is_oecd = 0;
+
+UPDATE migrant_remittances.migrant_remittances_in AS indata
+SET is_oecd = 1
+WHERE EXISTS (
+  SELECT * FROM migrant_remittances.oecd AS oecd
+  WHERE indata.country = oecd.Country
+  );
+
+SELECT *
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/migrant-remittances.csv'
+  FIELDS TERMINATED BY ','
+  OPTIONALLY ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+FROM migrant_remittances.migrant_remittances_in;
---
+++ sql/migrant-remittances/readme.txt
@@ -0,0 +1,7 @@
+This readme will assist in replication.
+
+File produced: migrant-remittances.csv
+
+OECD country data:                          http://www.oecd.org/about/membersandpartners/list-oecd-member-countries.htm
+Remittance data (migrant-remittance.xls):   http://www.cesifo-group.de/ifoHome/facts/DICE/Migration/Migration/Labour-Migration/migrant-remittance.html
+
