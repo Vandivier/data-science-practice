2a7de532917d3aa4dcd9546b3d3c428cb7c96107
Vandivier <vandivier_john@yahoo.com>
2017-04-14 23:48:51 -0400

    v1.0 processing script with output

--- js/process-names.js
+++ js/process-names.js
@@ -1,55 +1,89 @@
 //ref: https://coderwall.com/p/ohjerg/read-large-text-files-in-nodejs
 const fs = require('fs');
 const readline = require('readline');
-const stream = require('stream');
+const Stream = require('stream');
+const OS = require('os');
 
-const namesFolder = './names/';
-const outstream = new stream;
+const sOutfilePath = './unique-names.csv';
+const sNamesFolder = './names/';
+const streamOut = new Stream;
 let oKnownNames = {};
 
 /*
- * group 1 is by default. These files are csv with name in column 3
+ * group 1 is by default. These files are csv with name in column 4
  * group 2 files are space delimited with name in column 1
  * group 3 files are csv with name in column 1
  */
-fs.readdir(namesFolder, (err, files) => {
-  files.forEach(file => {
+let groupHandler = {};
+
+groupHandler['1'] = function(sPath, resolve) {
+  fStreamNameFile(sPath, ',', 3, resolve);
+}
+
+groupHandler['2'] = function(sPath, resolve) {
+  fStreamNameFile(sPath, ' ', 0, resolve);
+}
+
+groupHandler['3'] = function(sPath, resolve) {
+  fStreamNameFile(sPath, ',', 0, resolve);
+}
+
+function fStreamNameFile(sPath, sParseCharacter, iColumn, resolve) {
+  const streamIn = fs.createReadStream(sPath);
+  const _rl = readline.createInterface(streamIn, streamOut);
+
+  _rl.on('line', function(sLine) {
+    const sName = sLine.toLowerCase().split(sParseCharacter)[iColumn];
+    //console.log(sName);
+    oKnownNames[sName] = 1;               // value of 1 isn't special, it's just a performent way to ensure we get this name
+  });
+
+  _rl.on('close', ()=>{
+    //giConsoleUpdater.next();
+    resolve();
+  });
+}
+
+//ref: http://stackoverflow.com/a/18983245/3931488
+//ref: https://ryanpcmcquen.org/javascript/2015/10/25/map-vs-foreach-vs-for.html
+fs.readdir(sNamesFolder, (err, files) => {
+  let arrpFileProcesses = files.map(file => {
     var sGroupId = file.split('-')[1];
     var fGroupHandler = groupHandler[sGroupId];
-    var sPath = namesFolder + file;
+    var sPath = sNamesFolder + file;
 
-    if (fGroupHandler) {
-      fGroupHandler(sPath);
-    } else {
-      groupHandler['1'](sPath);
-    }
+    return new Promise((resolve) => {
+      if (fGroupHandler) {
+        fGroupHandler(sPath, resolve);
+      } else {
+        groupHandler['1'](sPath, resolve);
+      }
+    });
+  });
+
+  Promise.all(arrpFileProcesses).then(() => {
+    const streamWriteToFileSystem = fs.createWriteStream(sOutfilePath);
+    const sTitleLine = 'First or Last Names';
+    // TODO: column first and last name
+    // TODO: request from a names list, or loop name + i; i++ until 404
+    //    eg /dustinlongenecker public, /johnsmith2 404, /john and /john2 both exist (but private), /john3 exists and public with linkedIn.
+
+    // OS.EOL is a smart way to do a line break. ref: http://stackoverflow.com/questions/14173150/how-do-i-create-a-line-break-in-a-javascript-string-to-feed-to-nodejs-to-write-t
+    const sTextToWrite = [sTitleLine].concat(Object.keys(oKnownNames)).join(OS.EOL);
+
+    streamWriteToFileSystem.write(sTextToWrite);
+    process.exit(0);
   });
 });
 
-function fStreamNameFile(sPath) {
-  const instream = fs.createReadStream(sPath);
-  const _rl = readline.createInterface(instream, outstream);
-
-  _rl.on('line', function(line) {
-    // process line here
-    // parse name and add to oKnownNames
-  });
-
-  _rl.on('close', function() {
-    //console.log('done with ' + sPath);
-  });
+// after every batch of 10 files, update console.
+// generator pattern. ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+// TODO: it doesn't work. Fix or delete.
+function* giConsoleUpdaterFactory() {
+  //console.log('called');          //it only gets called once...?
+  let iFilesCompleted = 0;
+  iFilesCompleted++;
+  if(iFilesCompleted % 10 === 0) console.log('A batch of 10 files has completed.');
 }
 
-let groupHandler = {};
-
-groupHandler['1'] = function(sPath) {
-  fStreamNameFile(sPath);
-}
-
-groupHandler['2'] = function(sPath) {
-  console.log('burp2');
-}
-
-groupHandler['3'] = function(sPath) {
-  console.log('burp3');
-}
+let giConsoleUpdater = giConsoleUpdaterFactory();
