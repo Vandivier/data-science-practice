8d21293f9239144fc101e82e3871b4c69248e836
Vandivier <vandivier_john@yahoo.com>
2017-04-15 16:01:46 -0400

    clean extraneous code and comments

--- js/udacity-employment.js
+++ js/udacity-employment.js
@@ -1,4 +1,6 @@
 /*
+ *  run from cli like `node --harmony udacity-employment`
+ *
  *  Extract Udacity profiles, augment w data about are they employed, regress alternative credential value
  *  Install the package then run it, or just run it in https://runkit.com/
  *  Generates name combinations and attempts to look up Udacity users this way
@@ -15,22 +17,20 @@
  *  Extended analysis:
  *    Keep estimated gender, ethnicity, and state data, as available
  *    Augment with 2000s and 2010s: https://www2.census.gov/topics/genealogy/
+ *
+ *  ref: http://stackoverflow.com/questions/28739098/how-can-i-scrape-pages-with-dynamic-content-using-node-js
  */
 
-// TODO: some users have resumes, eg PDF, which has work history. Additional validation by checking for current employment via string '- present' || '- current' || '- 2017' || '-2017', etc
-// TODO: they also have 'work experiences' on the page we can scrape and also education
-// TODO: update package.json and such bc we can't use scrape-it on Udacity, pages are dynamic. http://stackoverflow.com/questions/28739098/how-can-i-scrape-pages-with-dynamic-content-using-node-js
-// TODO: get profile last updated date
+// TODO: check pdf resume for additional validation of current employment via string '- present' || '- current' || '- 2017' || '-2017', etc
 
-
-// use `node --harmony-async-await udacity-employment`
-// use `node --harmony udacity-employment`
 const phantom = require('phantom');
 const cheerio = require('cheerio');
 const async = require('async');
 
 const fsoNameSource = './process-names';
 const sUdacityBaseUrl = 'https://profiles.udacity.com/u/';
+
+const arrNames = ['john3', 'sara', 'sarah'];      // TODO: read from file. These names are seeds for usernames.
 
 /*  Test cases:
  *  /john3 is public with LinkedIn
@@ -69,13 +69,12 @@ async function fScrapeUdacityUser(sUsername, fCallback) {
   return fCallback(null, oUserObject);     // TODO: error handling. null means no error
 }
 
+// needed to bridge async.map with fScrapeUdacityUser
 function fScrapeUdacityUserSync(sUsername, fCallback) {
   return new Promise(function(resolve, reject) {
       resolve(fScrapeUdacityUser(sUsername, fCallback));
   });
 }
-
-const sUnrendered = '<div id="app"><noscript data-reactroot=""></noscript></div><script src="/js/manifest.cb664.js"></script><script src="/js/vendor.a4cb3.js"></script><script src="/js/app.05d50.js"></script>';
 
 //ref: http://stackoverflow.com/questions/31963804/how-to-scroll-in-phantomjs-to-trigger-lazy-loads?noredirect=1&lq=1
 // once each second, try to see if the body has rendered yet.
@@ -83,36 +82,12 @@ function getDynamicContentUdacity(page) {
   return new Promise(resolve => {
     setTimeout(() => {
       const sContent = page.evaluate(function () { return document.body.innerHTML; });
-
-      if (sContent !== null && sContent !== sUnrendered) {
-        resolve(sContent);
-        //return sContent;
-      } else {
-        console.log('still looking');
-        getDynamicContentUdacity(page);
-      }
-
+      if (sContent !== null && sContent !== sUnrendered) resolve(sContent);
     }, 1000);
   });
 }
 
-//fScrapeUdacityUser('john3', (err, returned)=>{console.log(returned)});
-let arrNames = ['john3', 'sara', 'sarah'];      // TODO: read from file. These names are seeds for usernames.
 // http://caolan.github.io/async/docs.html#map
 async.map(arrNames, fScrapeUdacityUserSync, function(err, arroUserObjects) {
   console.log(arroUserObjects);
 });
-
-var square = function (num, doneCallback) {
-  // Call back with no error and the result of num * num
-  return doneCallback(null, num * num);
-};
-
-// Square each number in the array [1, 2, 3, 4]
-async.map([1, 2, 3, 4], square, function (err, results) {
-  // Square has been called on each of the numbers
-  // so we're now done!
-  console.log("Finished!");
-  console.log(results);
-});
-
