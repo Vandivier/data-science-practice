e85479490c39a3db3c3b307aec869743542a0661
Vandivier <vandivier_john@yahoo.com>
2017-10-29 23:01:29 -0400

    sanity check

--- js/grand-tour-study/server.js
+++ js/grand-tour-study/server.js
@@ -1,6 +1,8 @@
 /**
  *  Description:
  *      Scrape data as defined in the readme from http://uci.ch/road/results/
+ *
+ *  At the time of writing, uci website has jQuery
  **/
 
 'use strict';
@@ -13,10 +15,14 @@ const puppeteer = require('puppeteer');
 const utils = require('./utils.js');
 
 const iChunkSize = 30;
-const iThrottleInterval = 2;
+const iThrottleInterval = 2; // seconds between batch of requests
 const sCommonBaseUrl = 'http://uci.ch/';
 const sRootUrl = sCommonBaseUrl + 'road/results/';
 const sResultDir = __dirname + '/results';
+
+const iFirstSeason = 2009;
+const iLastSeason = 2017;
+const arrsRaceSubstrings = ['Giro', 'Italia', 'Tour de France', 'Vuelta', 'Espa']; // TODO: not restrictive enough
 
 let wsMain;
 let wsErrorLog;
@@ -56,6 +62,7 @@ async function fpCollectBatchInformation(_arr) {
     return arrBatchResult;
 }
 
+// TODO: maybe not needed
 async function fGetDataByUrl(sUrl) {
     const page = await browser.newPage();
     let _$;
@@ -67,7 +74,38 @@ async function fGetDataByUrl(sUrl) {
     return _$('pre').text();
 }
 
+// note: this whole fucking method is a hack
+// not generalizable or temporally reliable in case of a site refactor
+async function fpGetSeasons(sUrl, iSeason) {
+    const _page = await browser.newPage();
+    let _$;
+
+    await page.goto(sUrl);
+    _$ = cheerio.load(await page.content());
+
+    //page.close();
+
+    _$('.uci-main-content .k-dropdown').last().click();     // open the seasons dropdown
+    _$('#seasons_listbox li').filter(function(){            // click the particular season
+        return this.textContent === String(iSeason)
+    })
+    .click();
+
+    // maybe wait some amount of time here to ensure page loads data...2 seconds?
+
+    return page;
+}
+
+// get each season page in parallel
+// traverse each page under the season in parallel (~21)
+// find strings of interest. open those pages. it's designed as SPA; may need to open new window
+// if using a new window, url looks like https://dataride.uci.ch/iframe/CompetitionResults/46187?disciplineId=10
+// for strings of interest, get general classification, points classification, and stage classification for each stage
+// these are a bunch of csvs; maybe download instead of write file (actually, that's equivalent)
 async function main() {
+    let arrvSeasons;
+    let _oPage;
+
     browser = await puppeteer.launch();
 
     if (!fs.existsSync(sResultDir)) {
@@ -76,16 +114,23 @@ async function main() {
 
     fSetWriters();
 
-    // get each season page in parallel
-    // traverse each page under the season in parallel (~21)
-    // find strings of interest. open those pages. it's designed as SPA; may need to open new window
-    // if using a new window, url looks like https://dataride.uci.ch/iframe/CompetitionResults/46187?disciplineId=10
-    // for strings of interest, get general classification, points classification, and stage classification for each stage
-    // these are a bunch of csvs; maybe download instead of write file (actually, that's equivalent)
+    // get an array of browser pages; one for each season
+    for (i = iFirstSeason; i < iLastSeason; i++;) {
+        _oPage = fpGetSeasons(sRootUrl, i);
+        arrpageSeasons.push(_oPage);
+    }
 
-    arroRenames = await fGetDataByUrl(sRootUrl);
-    arroRenames = JSON.parse(arroRenames);
-    //console.log('Rename check: ' + arroRenames.length);
+    console.log(arrpageSeasons);
+    //utils.settleAll
+    //sanity check
+    //console.log(arrpageSeasons);
+
+    /*
+    await utils.forEachThrottledAsync(iThrottleInterval, arrBatches, function (_arrBatch) {
+        console.log('batch process for batch # ' + (iCurrentBatch++) + ' of ' + arrBatches.length)
+        return fpCollectBatchInformation(_arrBatch);
+    });
+
     let arrBatches = utils.chunk(arroRenames, iChunkSize);
     //console.log('batch check: ' + arrBatches.length, arrBatches[0])
     //arrBatches = [arrBatches.pop(),[]];// testing with a subset
@@ -95,6 +140,7 @@ async function main() {
         console.log('batch process for batch # ' + (iCurrentBatch++) + ' of ' + arrBatches.length)
         return fpCollectBatchInformation(_arrBatch);
     });
+    */
 
     browser.close();
     process.exit();
