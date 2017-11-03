23582968a43f79a9948e18dfce32097056d5e978
Vandivier <vandivier_john@yahoo.com>
2017-11-02 22:49:29 -0400

    before parsing blocks, convert rtf to txt
    fpConvertRtfToTxt()
    wsIntermediate
    OSEOL

--- js/earhart-fellows/index.js
+++ js/earhart-fellows/index.js
@@ -6,35 +6,67 @@
 
 'use strict';
 
+const OSEOL = require('os').EOL;
+
 let fs = require('fs');
 let classReadLine = require('readline');
-var split = require('split')
+let split = require('split');
 
 let rsReadStream = fs.createReadStream('./EarhartFellowsMerged.rtf');
+let wsIntermediate = fs.createWriteStream('./intermediate.txt');
 let wsWriteStream = fs.createWriteStream('./output.csv');
 let regexDelimiter = /Graduate Fellowship(s)/;
 
 main();
 
-function main() {
+async function main() {
+    try {
+        await fpConvertRtfToTxt();
+    } catch (error) {
+        console.error('fpConvertRtfToTxt failed.\n');
+    }
+
+    fParseTxt();
+}
+
+// return a promise which indicates that txt is written
+function fpConvertRtfToTxt() {
     let rl = classReadLine.createInterface({
         input: rsReadStream
     });
 
     rl.on('line', (sLine) => {
         if (sLine.toLowerCase().includes('address')) { //address is for testing only
-            wsWriteStream.write(convertToPlain(sLine));
-            wsWriteStream.write('\n');
+            wsIntermediate.write(convertToPlain(sLine) + OSEOL);
         }
+    });
+
+    return fpStreamToPromise(rsReadStream);
+}
+
+function fParseTxt() {
+    let rl = classReadLine.createInterface({
+        input: rsReadStream
     });
 
     rsReadStream
         .pipe(split(regexDelimiter))
         .on('data', fHandleData)
+        .on('close', fNotifyEndProgram);
 }
 
 function fHandleData(sSplitData) {
-    console.log(sSplitData);
+    //wsWriteStream.write(convertToPlain(sSplitData));
+    let sParsedBlock = convertToPlain(sSplitData);
+
+    if (sParsedBlock.toLowerCase().includes('address')) { //address is for testing only
+        wsWriteStream.write(sParsedBlock);
+        wsWriteStream.write('\n');
+    }
+}
+
+function fNotifyEndProgram() {
+    console.log('Program completed.');
 }
 
 // ref: https://stackoverflow.com/questions/29922771/convert-rtf-to-and-from-plain-text
@@ -48,4 +80,12 @@ function convertToRtf(plain) {
 function convertToPlain(rtf) {
     rtf = rtf.replace(/\\par[d]?/g, "");
     return rtf.replace(/\{\*?\\[^{}]+}|[{}]|\\\n?[A-Za-z]+\n?(?:-?\d+)?[ ]?/g, "").trim();
+}
+
+// ref: https://github.com/petkaantonov/bluebird/issues/332
+function fpStreamToPromise(stream) {
+    return new Promise(function (resolve, reject) {
+        stream.on('end', resolve);
+        stream.on('error', reject);
+    });
 }
