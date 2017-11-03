bd741f6dfad5fd66f1c2b3c59cd8a23c92f834a0
Vandivier <vandivier_john@yahoo.com>
2017-11-02 20:51:17 -0400

    convert rtf initial commit;
    convertToRtf()
    convertToPlain()
    todo.csv

--- js/earhart-fellows/index.js
+++ js/earhart-fellows/index.js
@@ -1,10 +1,12 @@
 // ref: https://github.com/Vandivier/todo-to-csv/blob/master/index.js
 // ref: https://github.com/Vandivier/linklist-to-table/blob/master/index.js
+// below requires installing a cli tool; last resort
+// ref: https://github.com/dbashford/textract
 
 'use strict';
 let fs = require('fs');
 let classReadLine = require('readline');
-let rsReadStream = fs.createReadStream('./log.txt'); //todo: allow loop thru directory structure by list of allowed file extensions
+let rsReadStream = fs.createReadStream('./EarhartFellowsMerged.rtf'); //todo: allow loop thru directory structure by list of allowed file extensions
 let wsWriteStream = fs.createWriteStream('./todo.csv');
 
 
@@ -13,8 +15,21 @@ let rl = classReadLine.createInterface({
 });
 
 rl.on('line', (sLine) => {
-    if (sLine.substr(0,5).toLowerCase() === 'todo:') {
-        wsWriteStream.write(sLine);
+    if (sLine.toLowerCase().includes('address')) { //address is for testing only
+        wsWriteStream.write(convertToPlain(sLine));
         wsWriteStream.write('\n');
     }
 });
+
+// ref: https://stackoverflow.com/questions/29922771/convert-rtf-to-and-from-plain-text
+function convertToRtf(plain) {
+    plain = plain.replace(/\n/g, "\\par\n");
+    return "{\\rtf1\\ansi\\ansicpg1252\\deff0\\deflang2057{\\fonttbl{\\f0\\fnil\\fcharset0 Microsoft Sans Serif;}}\n\\viewkind4\\uc1\\pard\\f0\\fs17 " + plain + "\\par\n}";
+}
+
+// ref: https://stackoverflow.com/questions/29922771/convert-rtf-to-and-from-plain-text
+// not sure if it works w/ streams
+function convertToPlain(rtf) {
+    rtf = rtf.replace(/\\par[d]?/g, "");
+    return rtf.replace(/\{\*?\\[^{}]+}|[{}]|\\\n?[A-Za-z]+\n?(?:-?\d+)?[ ]?/g, "").trim();
+}
