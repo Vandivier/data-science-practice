bbd62d482dc746626ded3ca9348a2502bf8f3536
Mbjoerkh <33209918+Mbjoerkh@users.noreply.github.com>
2017-12-06 18:47:57 -0500

    Minor edits based on stata sorting

--- js/earhart-fellows/index.js
+++ js/earhart-fellows/index.js
@@ -59,7 +59,7 @@ const arrDegrees = ['Ph.D.', 'M.A.', 'MA.', 'M.B.A.', 'D.B.A.', 'B.A.'];
 const regexDelimiter = /Graduate Fellowship* *\(s\)/;
 const regexEmail = /[\S]+@[\S]+\.[, \S]+/g;
 
-let rsReadStream = fs.createReadStream('./EarhartFellowsMerged.txt');
+let rsReadStream = fs.createReadStream('./EarhartFellowsMergededited.txt');
 let wsWriteStream = fs.createWriteStream('./output.csv');
 let wsNonAdjacent = fs.createWriteStream('./non-adjacent-sponsor.txt');
 
