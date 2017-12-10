04f858414c6ece3a987898d1d2e8cefbb77746ba
Vandivier <vandivier_john@yahoo.com>
2017-12-10 17:48:46 -0500

    repec data initial commit

---
+++ js/earhart-fellows/email-append-repec.js
@@ -0,0 +1,200 @@
+/**
+ *  Description:
+ *      given an repec URL, get email address.
+ *
+ *  approach based on grand-tour-study, getsum.js
+ *  repec site assumed to have jQuery
+ **/
+
+'use strict';
+
+const Bluebird = require('bluebird'); // required twice so I can sometimes be explicit
+const cheerio = require('cheerio');
+const EOL = require('os').EOL;
+const fs = Bluebird.promisifyAll(require('fs'));
+const Promise = require('bluebird');
+const puppeteer = require('puppeteer');
+const util = require('util');
+
+const utils = require('./utils.js');
+
+const fpReadFile = util.promisify(fs.readFile);
+const fpWriteFile = util.promisify(fs.writeFile);
+
+const sRootUrl = 'https://dataride.uci.ch';
+const sResultDir = __dirname + '/results';
+const sInputCsvLocation = __dirname + '/repec.csv';
+const sOutputFileLocation = sResultDir + '/out-repec.csv';
+
+const oTitleLine = {
+    '_stack',
+    'name',
+    'web',
+    'count',
+    'email',
+    'affiliations',
+};
+
+let browser;
+let iCurrentInputRecord = 0;
+let iTotalInputRecords = 0;
+let sResultToWrite;
+let wsGotSome;
+let wsErrorLog;
+
+main();
+
+async function main() {
+    let sInputCsv;
+    let arrsInputRows;
+
+    browser = await puppeteer.launch();
+
+    if (!fs.existsSync(sResultDir)) {
+        fs.mkdirSync(sResultDir);
+    }
+
+    fSetWriters();
+
+    sInputCsv = await fpReadFile(sInputCsvLocation, 'utf8');
+    arrsInputRows = sInputCsv.split(EOL);
+
+    /** for testing only, shorten rows **/
+    arrsInputRows = arrsInputRows.slice(0, 50);
+    iTotalInputRecords = arrsInputRows.length;
+    console.log('early count, iTotalInputRecords = ' + iTotalInputRecords);
+    console.log('early count is typically overstated by a factor of ~20');
+    console.log('iTotalInputRecords / 20 = ' + (iTotalInputRecords / 20));
+    console.log('allow up to ~30s per scrape' + EOL);
+
+    await utils.forEachReverseAsyncPhased(arrsInputRows, function(sLineOfText, i) {
+        return fpHandleData(sLineOfText);
+    });
+
+    console.log('writing result file.');
+    sResultToWrite = fsScrapedDataToResult(oTitleLine) + EOL + sResultToWrite;
+    await fpWriteFile(sOutputFileLocation, sResultToWrite);
+    fEndProgram();
+}
+
+//  must ensure path exists before setting writers
+function fSetWriters() {
+    wsGotSome = fs.createWriteStream(sOutputFileLocation);
+    wsErrorLog = fs.createWriteStream(sResultDir + '/errors.txt');
+}
+
+// TODO: maybe wait on condition instead of time
+// eg using page.mainFrame().waitForSelector
+async function fpWait() {
+    return new Promise((resolve) => setTimeout(() => resolve(undefined), 2));
+}
+
+// don't write the title line as it appears many times
+// we will append just once manually
+// also, don't write empty lines
+// TODO: click go to next button and get more stages
+function fpHandleData(sLineOfText) {
+    const arrsCellText = sLineOfText.split(',');
+    const sUrl = '';
+
+    if (sUrl) {
+        return fpScrapeInputRecord(sUrl)
+            .then(function (oScraped) {
+                let oFullData = Object.assign(oScraped, oOriginalData);
+
+                iCurrentInputRecord++;
+                console.log('scraped input record #: '
+                            + iCurrentInputRecord
+                            + '/' + iTotalInputRecords
+                            + EOL);
+
+                sResultToWrite += (fsScrapedDataToResult(oFullData) + EOL);
+                return Promise.resolve();
+            })
+            .catch(function (reason) {
+                console.log('fpHandleData err: ', reason);
+            });
+    }
+
+    iTotalInputRecords--;
+    return Promise.resolve();
+}
+
+function fEndProgram() {
+    browser.close();
+    console.log('Processes completed.');
+    process.exit();
+}
+
+// returns a page which has been navigated to the specified season page
+// note: this whole fucking method is a hack
+// not generalizable or temporally reliable in case of a site refactor
+// target site includes jQuery already. _$ is cheerio, $ is jQuery
+async function fpScrapeInputRecord(sUrl) {
+    const _page = await browser.newPage();
+    let executionContext;
+    let _$;
+    let pageWorkingCompetitionPage;
+    let poScrapeResult;
+
+    await _page.goto(sUrl, {
+        'networkIdleTimeout': 10000,
+        'waitUntil': 'networkidle',
+        'timeout': 0
+    }); // timeout ref: https://github.com/GoogleChrome/puppeteer/issues/782
+
+    _$ = cheerio.load(await _page.content());
+    _page.on('console', _fCleanLog); // ref: https://stackoverflow.com/a/47460782/3931488
+
+    executionContext = _page.mainFrame().executionContext();
+    poScrapeResult = await executionContext.evaluate((_iCurrentInputRecord) => {
+        return _fpWait(900)
+            .then(function () {
+                let sEmail = $('.emaillabel').parent().find('td span').text();
+                let arroAffiliations = [];
+
+                return {
+                    sEmail,
+                    arroAffiliations,
+                };
+            })
+            .catch(function (err) {
+                console.log('fpScrapeInputRecord err: ', err);
+            });
+
+        // larger time allows for slow site response
+        // some times of day when it's responding fast u can get away
+        // with smaller ms; suggested default of 12.5s
+        function _fpWait(ms) {
+            ms = ms || 10000;
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+    });
+
+    _page.close();
+    return poScrapeResult;
+
+    function _fCleanLog(ConsoleMessage) {
+        console.log(ConsoleMessage.text + EOL);
+    }
+}
+
+// like String.trim()
+// but, removes commas and quotes too (outer or interior)
+function fsTrimMore(s) {
+    return s && s.replace(/[,"]/g, '').trim();
+}
+
+// ref: earhart-fellows, fsRecordToCsvLine
+// TODO: generic object-to-csv-row
+function fsScrapedDataToResult(oStageData) {
+    let sToCsv = ''
+                + '"' + oStageData._stack + '",'
+                + '"' + oStageData.name + '",'
+                + '"' + oStageData.web + '",'
+                + '"' + oStageData.count + '",'
+                + '"' + oStageData.email + '",'
+                + '"' + oStageData.affiliations + '"'
+
+    return sToCsv;
+}
