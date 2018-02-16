2911857f3d3e931c9adefb061e34ca58280892c4
Vandivier <vandivier_john@yahoo.com>
2018-02-15 19:43:24 -0500

    one space before BFR

--- python/udacity-scrapy/quotesbot/spiders/toscrape-css.py
+++ python/udacity-scrapy/quotesbot/spiders/toscrape-css.py
@@ -1,22 +1,31 @@
 # -*- coding: utf-8 -*-
+import re
 import scrapy
 
-
 class ToScrapeCSSSpider(scrapy.Spider):
-    name = "toscrape-css"
+    name = "scrape"
     start_urls = [
-        'http://quotes.toscrape.com/',
+        'https://en.wikipedia.org/wiki/Falcon_Heavy',
     ]
 
     def parse(self, response):
-        for quote in response.css("div.quote"):
+        regex = re.compile(r" +", flags=re.MULTILINE)
+
+        for i, quote in enumerate(response.css("p")):
+            arrText = quote.xpath('.//text()').extract()
+            sAllText = ' '.join(arrText)
+            sPostProcessed = regex.sub(" ", sAllText)
+            #re.sub( '\s+', ' ', mystring ).strip()
+
             yield {
-                'text': quote.css("span.text::text").extract_first(),
-                'author': quote.css("small.author::text").extract_first(),
-                'tags': quote.css("div.tags > a.tag::text").extract()
+                'anchorCount': '',
+                'index': i,
+                'firstWord': '',
+                'text': sPostProcessed
             }
 
+            '''
         next_page_url = response.css("li.next > a::attr(href)").extract_first()
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
-
+            '''
