ca1855b826ddf58c92b7a352d8ba2dc77f2bbb5c
Vandivier <vandivier_john@yahoo.com>
2018-02-19 11:16:45 -0500

    calc % employed
    TODO: fix list comprehension

--- python/udacity-postprocess/main.py
+++ python/udacity-postprocess/main.py
@@ -2,9 +2,22 @@ import json
 from pprint import pprint
 import requests
 
-sCacheUrl = 'https://raw.githubusercontent.com/Vandivier/udacity-apify/master/kv-store-dev/OUTPUT'
+def main():
+    sCacheUrl = 'https://raw.githubusercontent.com/Vandivier/udacity-apify/master/kv-store-dev/OUTPUT'
 
-response = requests.get(sCacheUrl)
-data = json.load(response.json())
+    response = requests.get(sCacheUrl)
+    s = response.text
+    data = json.loads(s)
+    listoPeople = data['people']
 
-pprint(data['proxies'])
+    # list comprehension > lambda + filter as readability best practice
+    # ref: https://stackoverflow.com/questions/3013449/list-filtering-list-comprehension-vs-lambda-filter
+    #listoEmploymentMeasured = [o for o in listoPeople if hasattr(o, 'bPresentlyEmployed')]
+    listoEmploymentMeasured = [o for o in listoPeople if True]
+    #listoEmployed = [o for o in listoEmploymentMeasured if o['bPresentlyEmployed'] == True]
+    listoEmployed = [o for o in listoEmploymentMeasured if False]
+    iPercentEmployed = len(listoEmployed) / len(listoEmploymentMeasured)
+
+    pprint(iPercentEmployed)
+
+main()
