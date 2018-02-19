1a1b2df29a32f5b941fdfce57c26891f5113bd11
Vandivier <vandivier_john@yahoo.com>
2018-02-19 12:53:06 -0500

    use 2-touple

--- python/udacity-postprocess/main.py
+++ python/udacity-postprocess/main.py
@@ -8,19 +8,21 @@ def main():
     response = requests.get(sCacheUrl)
     s = response.text
     data = json.loads(s)
-    listoPeople = data['people']
+
+    # a touple is a list of lists. An n-tuple is a sequence (or ordered list) of n elements. 2tpl is a 2 touple
+    # ref: https://stackoverflow.com/questions/1679384/converting-python-dictionary-to-list
+    tpl2People = data['people'].items()
 
     # list comprehension > lambda + filter as readability best practice
     # ref: https://stackoverflow.com/questions/3013449/list-filtering-list-comprehension-vs-lambda-filter
-    #listoEmploymentMeasured = [o for o in listoPeople if hasattr(o, 'bPresentlyEmployed')]
-    listoEmploymentMeasured = [o for o in listoPeople if True]
-    #listoEmployed = [o for o in listoEmploymentMeasured if o['bPresentlyEmployed'] == True]
-    listoEmployed = [o for o in listoEmploymentMeasured if False]
+    listoEmploymentMeasured = [v for k,v in tpl2People if 'bPresentlyEmployed' in v]
+    listoEmployed = [o for o in listoEmploymentMeasured if o['bPresentlyEmployed'] == True]
     iPercentEmployed = len(listoEmployed) / len(listoEmploymentMeasured)
 
     pprint("Percent employed: " + str(iPercentEmployed))
+    pprint("Note: percent employed is comparable to an age-nonadjusted employment-population ratio.") # ref: https://data.bls.gov/timeseries/LNS12300000
     pprint("Sample size with employment data: " + str(len(listoEmploymentMeasured)))
-    pprint("Total sample size: " + str(len(listoPeople)))
-    pprint("Percent of observations with employment data: " + str(len(listoEmploymentMeasured) / len(listoPeople)))
+    pprint("Total sample size: " + str(len(tpl2People)))
+    pprint("Percent of observations with employment data: " + str(len(listoEmploymentMeasured) / len(tpl2People)))
 
 main()
