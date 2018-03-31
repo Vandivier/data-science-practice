7213c010478540c0f248a015f5ccf2dea690a8cc
Vandivier <vandivier_john@yahoo.com>
2018-03-31 01:52:37 -0400

    complete except for download

--- stata/udacity-exploratory-analysis/data-miner/console-scraper.js
+++ stata/udacity-exploratory-analysis/data-miner/console-scraper.js
@@ -1,46 +1,58 @@
 // ref: vandivier/github/udacity-apify/main.js
 
+var bDownloadTextResult = true; // if false just print result to web console.
+
 (async function (_iCurrentInputRecord) {
     const script = document.createElement('script') // inject jQuery
     script.src = 'https://code.jquery.com/jquery-3.3.1.js'; // inject jQuery
     document.getElementsByTagName('head')[0].appendChild(script); // inject jQuery
     console.log('scraping: ' + window.location.href);
 
-    // toast message will disappear if you wait too long
     await _fpWait(1000);
 
-    var arr$Affiliations = $('#affiliation-body a[name=subaffil]');
-    var sarrAffiliations = '';
     var _oResult = {
         'bUserExists': $('[class*=profile-container]').length > 0,
         'bProfileIsPrivate': $('[class*="toast--message"]').html() === 'Profile is private',
         'bTooManyRequestsError': _fsSafeTrim($('[class*="toast--message"]').html()) === 'Too many requests'
     };
 
+    document.querySelector('[class*="educations--more--"').click();
+    document.querySelector('[class*="works--more--"').click();
+
     // wait a bit longer for UI to render if it is a valid scrape target
     if (!_oResult.bProfileIsPrivate &&
         !_oResult.bTooManyRequestsError) {
-        await _fpWait(4000);
 
+        _oResult.sProfileLastUpdate = $('div[class*="profile--updated"]').text();
         _oResult.sUserName = $('h1[class*="user--name"]').html();
         _oResult.sLocation = $('[alt="location marker"]').next().html();
-        _oResult.sEmail = $('.emaillabel').parent().find('td span').text();
+        _oResult.sEmail = $('[alt="email"]').parent().text();
+        _oResult.sStateOrCountry = _oResult.sLocation
+            && _oResult.sLocation.split(', ').length > 1
+            && _oResult.sLocation.split(', ')[1];
+
+        _oResult.sImageUrl = $('[class*=user--photo--]')[0].src;
+        _oResult.sLinkedInUrl = $('a[title="LINKEDIN"]').attr('href');
+        _oResult.sGitHubUrl = $('a[title="GITHUB"]').attr('href');
+        _oResult.sTwitterUrl = $('a[title="TWITTER"]').attr('href');
+        _oResult.sResumeUrl = $('a[title="Resume"]').attr('href');
+
+        _oResult.sExperienceHtml = $('[class*=works--section--]').html();
+        _oResult.sEducationHtml = $('[class*=educations--section--]').html();
+        _oResult.iCountOfNanodegrees = $('[class*=_degree--nanodegree]').length;
         _oResult.iEducationCount = $('div[class*="educations--section"] div[class*="index--education"]').length;
         _oResult.iExperienceCount = $('div[class*="works--section"] div[class*="_work--work"]').length;
-        _oResult.sLinkedInUrl = $('a[title="LINKEDIN"]').attr('href');
-        _oResult.sResumeUrl = $('a[title="Resume"]').attr('href');
-        _oResult.bOtherError = false;
-        _oResult.bPresentlyEmployed = $('div[class*="works--section"] div[class*="_work--work"] span[class*="_work--present"]').length > 0;
-        _oResult.sProfileLastUpdate = $('div[class*="profile--updated"]').text();
+        _oResult.bPresentlyEmployed = $('div[class*="works--section"] span[class*="date--present"]').length > 0;
 
-        arr$Affiliations && arr$Affiliations.each(function (arr, el) {
-            let sTrimmed = _fsSafeTrim(el.innerText.replace(/\s/g, ' '));
-            _oResult.sarrAffiliations += ('~' + sTrimmed);
-        });
+        _oResult.sLanguagesSpoken = $('[class*=work-preferences--language--]').text();
     }
 
-    //return Promise.resolve(_oResult);
-    console.log(_oResult);
+    if (bDownloadTextResult) {
+        
+    }
+    else {
+        console.log(_oResult);
+    }
 
     // larger time allows for slow site response
     // some times of day when it's responding fast u can get away
